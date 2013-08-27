c5302ba9fe78969035cd428248c5c25165d8f85a
Julian Ceballos <julian@pikhub.com>
2013-08-27 03:13:35 -0500

    modelo version agregado y corregidos unos detalles

--- api/models.js
+++ api/models.js
@@ -11,16 +11,8 @@ models.User = new willy.Model('User', {
   active: { type: Boolean, default: true }
 });
 
-models.Project = new willy.Model('Project', {
+models.Version = new willy.Model('Version', {
   name: { type: String, required: true },
-  description: { type: String, required: true, default: '' },
-  versions: [models.Versions],
-  milestones: [models.Milestones],
-  components: [models.Components],
-  issues: [models.Issues],
-  readers: [models.Users],
-  writers: [models.Users],
-  admins: [models.Users],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   active: { type: Boolean, default: true }
@@ -30,14 +22,14 @@ models.Milestone = new willy.Model('Milestone', {
   name: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  active: { type: Boolean, default: true }  
+  active: { type: Boolean, default: true }
 });
 
 models.Component = new willy.Model('Component', {
   name: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  active: { type: Boolean, default: true }  
+  active: { type: Boolean, default: true }
 });
 
 models.Status = new willy.Model('Status', {
@@ -45,7 +37,7 @@ models.Status = new willy.Model('Status', {
   icon: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  active: { type: Boolean, default: true }  
+  active: { type: Boolean, default: true }
 });
 
 models.Kind = new willy.Model('Kind', {
@@ -53,7 +45,7 @@ models.Kind = new willy.Model('Kind', {
   icon: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  active: { type: Boolean, default: true }  
+  active: { type: Boolean, default: true }
 });
 
 models.Priority = new willy.Model('Priority', {
@@ -61,17 +53,32 @@ models.Priority = new willy.Model('Priority', {
   icon: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  active: { type: Boolean, default: true }  
+  active: { type: Boolean, default: true }
 });
 
 models.Issue = new willy.Model('Issue', {
   name: { type: String, required: true },
   description: { type: String, required: true },
   attach: { type: String, required: true },
-  status: models.Status,
-  kind: models.Kind,
-  priority: models.Priority,
-  response: models.User,
+  status: { type: Schema.Types.ObjectId, ref: 'Status' },
+  kind: { type: Schema.Types.ObjectId, ref: 'Kind' },
+  priority: { type: Schema.Types.ObjectId, ref: 'Priority' },
+  responsable: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  active: { type: Boolean, default: true }
+});
+
+models.Project = new willy.Model('Project', {
+  name: { type: String, required: true },
+  description: { type: String, required: true, default: '' },
+  versions: [models.Version],
+  milestones: [models.Milestone],
+  components: [models.Component],
+  issues: [models.Issue],
+  readers: [models.User],
+  writers: [models.User],
+  admins: [models.User],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   active: { type: Boolean, default: true }
